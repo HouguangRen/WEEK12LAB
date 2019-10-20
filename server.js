c5598f25b7c8d6c3a2572a9ce78955e5f0ed8a61
Houguang Ren <hren27@student.monash.edu.au>
2019-10-20 19:45:14 +1100

    init

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ let app = express();
 let server = require("http").Server(app);
 let io = require("socket.io")(server);
 let port = 8888;
+// let name="";
 app.use("/", express.static(path.join(__dirname, "dist/chatApp")));
 io.on("connection", socket => {
   console.log("new connection made from client with ID="+socket.id);
+  // socket.on("newName", data => {
+  //   io.sockets.emit("name", { name: data});
+  // });
   socket.on("newMsg", data => {
-    io.sockets.emit("msg", { msg: data, timeStamp: getCurrentDate() });
+    io.sockets.emit("msg", { msg: data.m, name: data.n,timeStamp: getCurrentDate() });
   });
+
 });
 server.listen(port, () => {
   console.log("Listening on port " + port);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,36 @@ import * as io from "socket.io-client";
 })
 export class AppComponent {
   messageText:String="";
+  nameText:String="";
   messages: Array<any> = [];
+  // names: Array<any> = [];
+  // d1:String="";
+  // d2:String="";
+  // arr:Array<any>[][];
   socket: SocketIOClient.Socket;
   constructor() {
     this.socket = io.connect();
   }
   ngOnInit() {
     this.messages = new Array();
+    // this.names = new Array();
     this.listen2Events();
   }
   listen2Events() {
     this.socket.on("msg", data => {
       this.messages.push(data);
+      // this.d1=data;
     });
+    // this.socket.on("name", data => {
+    //   this.names.push(data);
+    //   this.d2=data;
+    // });
+    // this.arr.push([this.d1,this.d2]);
   }
   sendMessage() {
-    this.socket.emit("newMsg", this.messageText);
+    this.socket.emit("newMsg", {m:this.messageText,n:this.nameText});
     this.messageText = "";
+    // this.socket.emit("newName", this.nameText);
+    this.nameText="";
   }
 }
